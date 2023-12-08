942aa77602ceeb61a71be56b06e57457461918e5
n0sebleeded <devDanilAkhunzianov@outlook.com>
2023-12-09 01:53:50 +0300

    Ships placement visualisation

--- ts-solution/placeShips.ts
+++ ts-solution/placeShips.ts
@@ -5,6 +5,6 @@ import { setupShip, localShips } from "./setupBoard.js";
 let matrix:Board = setupShip(localShips[0], startGameBoard);
 for (let i = 1; i < localShips.length; i++) {
     matrix = setupShip(localShips[i], matrix);
-    console.log(i);
 }
-ShowBoard(matrix);
+
+export default matrix;
