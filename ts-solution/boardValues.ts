632a17872fb21222546b2ab4cec2ed541b532a2d
n0sebleeded <devDanilAkhunzianov@outlook.com>
2023-12-26 01:35:23 +0300

    hidden hit and miss states

--- ts-solution/boardValues.ts
+++ ts-solution/boardValues.ts
@@ -1,6 +1,8 @@
 import matrix from "./placeShips.js";
+import {CellState} from "./types";
 
 const cells:NodeListOf<Element> = document.querySelectorAll('.board td');
+const cellsSecondBoard: NodeListOf<Element> = document.querySelectorAll('.boardsec td');
 console.log('test');
 
 cells.forEach((cell:Element, index:number):void => {
@@ -8,12 +10,28 @@ cells.forEach((cell:Element, index:number):void => {
     const row = Math.floor(index / 10); // 10 - количество ячеек в строке
     const col = index % 10;
 
+    const cellState: CellState = matrix[row][col].state;
+
     // @ts-ignore
-    cell.className = "state-" + matrix[row][col].state;
+    cell.state = cellState;
+
     cell.addEventListener('click', (event: Event) => {
-        if (cell.className == "state-1") {
+        if (cellState == 1) {
             cell.className = "state-hit";
         }
+        else {
+            cell.className = "state-miss";
+        }
     });
+});
 
+cellsSecondBoard.forEach((cell:Element, index:number):void => {
+    // Получаем индекс строки и столбца на основе индекса ячейки
+    const row = Math.floor(index / 10); // 10 - количество ячеек в строке
+    const col = index % 10;
+
+    const cellState: CellState = matrix[row][col].state;
+
+    // @ts-ignore
+    cell.className = "state-" + matrix[row][col].state;
 });
