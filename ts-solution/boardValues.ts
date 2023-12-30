5fbabbd51396c242b72ca240ae09a298b0423190
n0sebleeded <devDanilAkhunzianov@outlook.com>
2023-12-30 18:15:38 +0300

    primitive bot logic

--- ts-solution/boardValues.ts
+++ ts-solution/boardValues.ts
@@ -1,35 +1,56 @@
-/*import { matrix, matrixPlayer } from "./placeShips.js";*/
 import {matrix, matrixPlayer} from "./placeShips.js";
 import {CellState} from "./types";
 
 const cells:NodeListOf<Element> = document.querySelectorAll('.board td');
 const cellsSecondBoard: NodeListOf<Element> = document.querySelectorAll('.boardsec td');
+let playerCanAttack = true;
 
-cells.forEach((cell:Element, index:number):void => {
-    /*// Получаем индекс строки и столбца на основе индекса ячейки
-    const row = Math.floor(index / 10); // 10 - количество ячеек в строке
+const clickHandler = (event: Event) => {
+    const clickedCell = event.target as Element;
+    const index = Array.from(cells).indexOf(clickedCell);
+    const row = Math.floor(index / 10);
     const col = index % 10;
-
     const cellState: CellState = matrix[row][col].state;
 
-    // @ts-ignore
-    cell.state = cellState;
+    if (cellState === 1) {
+        clickedCell.className = "state-hit";
+    } else {
+        clickedCell.className = "state-miss";
+    }
 
-    cell.addEventListener('click', (event: Event) => {
-        if (cellState == 1) {
-            cell.className = "state-hit";
+    cells.forEach((cell) => {
+        cell.removeEventListener('click', clickHandler);
+    });
+
+    playerCanAttack = false;
+    // Bot's turn
+    setTimeout(() => {
+        let randRow = Math.floor(Math.random() * 9);
+        let randCol = Math.floor(Math.random() * 9);
+
+        const botCell = cellsSecondBoard[randRow * 10 + randCol];
+        const botCellState = matrixPlayer[randRow][randCol].state;
+
+        if (botCellState === 1) {
+            botCell.className = "state-hit";
+        } else {
+            botCell.className = "state-miss";
         }
-        else {
-            cell.className = "state-miss";
-        }
-    });*/
-    const row = Math.floor(index / 10); // 10 - количество ячеек в строке
-    const col = index % 10;
 
-    const cellState: CellState = matrix[row][col].state;
+        // Player can attack again after bot's turn
+        playerCanAttack = true;
+        cells.forEach((cell) => {
+            if (playerCanAttack) {
+                cell.addEventListener('click', clickHandler);
+            }
+        });
+    }, 1000);
+};
 
-    // @ts-ignore
-    cell.className = "state-" + cellState;
+cells.forEach((cell) => {
+    if (playerCanAttack) {
+        cell.addEventListener('click', clickHandler);
+    }
 });
 
 cellsSecondBoard.forEach((cell:Element, index:number):void => {
